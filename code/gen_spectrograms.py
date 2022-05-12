8e0a0d4941fea8a682f7ea82e3d92fe1c3b461c8
jonas <jonas.kittelberger@gmail.com>
2022-05-12 11:36:52 +0200

    deleted unused imports causing errros

--- code/gen_spectrograms.py
+++ code/gen_spectrograms.py
@@ -2,20 +2,9 @@ import math
 import os
 import torch
 import torchaudio
-import IPython.display as ipd
 import matplotlib
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
 from pathlib import Path
-import IPython.display as ipd
-import numpy as np
-import pandas as pd
-import librosa
-from tqdm import tqdm
-from sklearn.preprocessing import StandardScaler
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import Adam
 from matplotlib.transforms import Bbox
 
 
